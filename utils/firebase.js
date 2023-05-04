826aeaa6e3d3be8eaea88d6669368369f9aab608
Ryan Haraki <54621020+RyanHaraki@users.noreply.github.com>
2023-05-03 22:51:22 -0700

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -13,12 +13,6 @@ const firebaseConfig = {
   // storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
   // messagingSenderId: process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID,
   // appId: process.env.NEXT_FIREBASE_APP_ID,
-  apiKey: "AIzaSyCQ7AaVP8lFYdocFQ2yvhVrnaS_3HLF9Yc",
-  authDomain: "produhacks-818cb.firebaseapp.com",
-  projectId: "produhacks-818cb",
-  storageBucket: "produhacks-818cb.appspot.com",
-  messagingSenderId: "159987897505",
-  appId: "1:159987897505:web:e9cc19884cc53a4f0b0ce0",
 };
 
 // Initialize Firebase
